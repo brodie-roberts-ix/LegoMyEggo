3bec04db0d8d3c2d178173de0e1f5fe2b0c995c7
Brodie Roberts <brodie.roberts@indexexchange.com>
2019-09-30 16:50:29 -0400

    Refactored for alpha release [LEGGO-3660]

--- adventure.go
+++ adventure.go
@@ -10,20 +10,28 @@ import (
 	"github.com/brodie-roberts-ix/LegoMyEggo/leggo"
 )
 
+var storyFile = "stories/demo.json"
+
 func main() {
-	file, err := os.Open("../stories/derp.json")
+	if len(os.Args) > 1 {
+		storyFile = os.Args[1]
+	}
+
+	file, err := os.Open(storyFile)
 	tableflip(err)
 	data, err := ioutil.ReadAll(file)
 	tableflip(err)
 
 	game, err := leggo.NewFromJson(data)
 	tableflip(err)
+	fmt.Printf("Loading game file: %s\n", storyFile)
 
 	var msg string
 	var actions []string
 	msg, actions = game.Display()
+	fmt.Println()
 	fmt.Println(msg)
-	fmt.Println("Actions: {", strings.Join(actions, " | "), "}")
+	fmt.Println("  {", strings.Join(actions, " | "), "}\n")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for !game.HaveWon() {
@@ -31,12 +39,22 @@ func main() {
 		if !scanner.Scan() {
 			break
 		}
-		msg, actions = game.Act(scanner.Text())
+		line := scanner.Text()
+		if line == "" {
+			continue
+		}
+		msg, actions = game.Act(line)
+		fmt.Println()
 		fmt.Println(msg)
-		fmt.Println("Actions: {", strings.Join(actions, " | "), "}")
+		fmt.Println("  {", strings.Join(actions, " | "), "}\n")
 	}
 	tableflip(scanner.Err())
-	fmt.Println("VICTORY!!!")
+
+	if game.HaveWon() {
+		fmt.Println("VICTORY!!!")
+	} else {
+		fmt.Println("Goodbye.")
+	}
 }
 
 func tableflip(err error) {
